67674c4a0918d6d167b24e3c089cdec4dd510112
Alexey Ponkratov <super.ponkrat2014@yandex.ru>
2022-10-30 00:47:05 +0300

    DTO refactor

--- app/src/main/java/com/ponkratov/weatherapp/data/mapper/CityMapper.kt
+++ app/src/main/java/com/ponkratov/weatherapp/data/mapper/CityMapper.kt
@@ -5,23 +5,18 @@ import com.ponkratov.weatherapp.data.model.CityEntity
 import com.ponkratov.weatherapp.domain.model.City
 
 fun CitiesResponse.toDomainList(): List<City> {
-    val citiesList = mutableListOf<City>()
-    results.forEach {
-        citiesList.add(
-            City(
-                it.id,
-                it.name,
-                it.country,
-                it.countryCode,
-                it.latitude,
-                it.longitude,
-                it.elevation,
-                it.timezone
-            )
+    return results.map {
+        City(
+            it.id,
+            it.name,
+            it.country,
+            it.countryCode,
+            it.latitude,
+            it.longitude,
+            it.elevation,
+            it.timezone
         )
     }
-
-    return citiesList
 }
 
 fun CityEntity.toDomain(): City {
